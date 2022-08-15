ddb2c1db058e6379eb4eb812850a951060a51ffc
Patrick Aziken <onaziken@gmail.com>
2022-08-15 17:10:17 +0100

    Coding Challenge 2 - Used template literal - Used if-else statement

--- script.js
+++ script.js
@@ -53,3 +53,28 @@ console.log(johnBMI2);
 //Check if Mark has higher BMI
 const markHigherBMI2 = markBMI2 > johnBMI2;
 console.log(markHigherBMI2);
+
+////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
+
+// Print to console
+console.log("Mark's BMI is higher than John's!");
+console.log("John's BMI is higher than Mark's!");
+
+// Using template literals and if-else statement
+if (markBMI1 > johnBMI1) {
+    console.log(`Mark's BMI (${markBMI1}) is higher than John's (${johnBMI1})`);
+} else {
+    console.log(`Mark's BMI (${markBMI2}) is lower than John's (${johnBMI2})`);
+}
