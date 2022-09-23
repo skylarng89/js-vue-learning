588ec5f333bff828999c4e4730b73359f590bd3b
Patrick Aziken <onaziken@gmail.com>
2022-09-24 00:25:04 +0100

    New: Sound Effect & High Score - Fix: Fixed bugs with DOM manipulation in different states - Fix: Bad logic with zero score, wrong guesses - Fix: Correct guess not displaying in placeholder - Add: Sound effects on correct or wrong guesses - Add: Highscore - Add: Reset Game functionality - Add: Disable/Enable "Check" button for correct guess state

--- 04_the_numbers_game/script.js
+++ 04_the_numbers_game/script.js
@@ -1,66 +1,134 @@
-'use strict';
+"use strict";
 
-// Store random number from 1 - 20
-const cpuNum = Math.trunc(Math.random() * 20) + 1;
+// Store random number from 1 - 20 in cpu
+let cpuNum = Math.trunc(Math.random() * 30) + 1;
 
 // Store initial score
-let playerScore = 20;
+let playerScore = 100;
+const printScore = () =>
+  (document.getElementsByClassName("ulc-player-score")[0].textContent =
+    playerScore);
+printScore();
+
+// Set highscore
+let highScore = 0;
 
 // Set guessed number placeholder
-document.querySelector('.ulc-number').textContent = '?';
+let guessPlaceholder = (document.getElementsByClassName(
+  "ulc-number"
+)[0].textContent = "?");
 
+// Set messages
+const correctGuessMsg = () =>
+  (document.getElementById("ulc-play-msg").textContent = "Correct Guess!!! 🥳");
+const emptyNumMsg = () =>
+  (document.getElementById("ulc-play-msg").textContent = "No number! 😜");
+const lowNumMsg = () =>
+  (document.getElementById("ulc-play-msg").textContent = "Yikes!!! Too Low 🥴");
+const highNumMsg = () =>
+  (document.getElementById("ulc-play-msg").textContent =
+    "Yikes!!! Too High 😬");
+const gameOverMsg = () =>
+  (document.getElementById("ulc-play-msg").textContent = "Game over! ❌");
+
+// Set game background colour on guess
+const correctGuessBg = function () {
+  document.getElementsByTagName("body")[0].style.backgroundColor = "#20e7a7";
+  document.getElementsByTagName("body")[0].style.color = "#212121";
+};
+
+const normalBgColor = function () {
+  document.getElementsByTagName("body")[0].style.backgroundColor = "#212121";
+  document.getElementsByTagName("body")[0].style.color = "#ffffff";
+};
+
+// Increment||Decrement player score on correct guess
+const addScore = () => (playerScore += 5);
+const subScore = () => (playerScore -= 5);
+
+// Disable/Enable Check button
+const disableCheckBtn = function () {
+  document.querySelectorAll("button.check")[0].disabled = true;
+  document.querySelectorAll("button.check")[0].style.backgroundColor =
+    "#f5f5f5";
+  document.querySelectorAll("button.check")[0].style.color =
+    "#a5a5a5";
+};
+const enableCheckBtn = function () {
+  document.querySelectorAll("button.check")[0].disabled = false;
+  document.querySelectorAll("button.check")[0].style.backgroundColor =
+    "var(--ulc-blue)";
+  document.querySelectorAll("button.check")[0].style.color =
+    "var(--ulc-white)";
+}
+
+const correctSound = new Audio("./media/audio/correct.mp3");
+// correctSound.play();
+
+const wrongSound = new Audio("./media/audio/wrong.mp3");
+// wrongSound.play();
 
 // Get click event on button, pass as function exp to event listener
-document.querySelector('.check').addEventListener('click', function () {
-    const guessNum = Number(document.querySelector('#ulc-num-input').value);
-
+document
+  .getElementsByClassName("check")[0]
+  .addEventListener("click", function () {
+    // Read player input
+    let guessNum = Number(document.getElementById("ulc-num-input").value);
     if (!guessNum) {
-        document.querySelector('#ulc-play-msg').textContent = 'No number! 😜';
-    } else if (guessNum === cpuNum) {
-        playerScore++
-        document.querySelector('#ulc-play-msg').textContent = 'Correct Guess!!! 🥳';
-
-        document.querySelector('body').style.backgroundColor = 'green';
-        
-        document.querySelector('.ulc-number').style.width = '30rem';
-
-        document.querySelector('.ulc-number').textContent = playerScore;
-    } else if (guessNum > cpuNum) {
-        // Run only if playerScore is greater than zero
-        if (playerScore > 1) {
-            document.querySelector('#ulc-play-msg').textContent = 'Yikes!!! Too High 😬';
-            // Decrease score if guess is wrong
-            playerScore--
-            document.querySelector('.ulc-player-score').textContent = playerScore;
-        } else {
-            document.querySelector('#ulc-play-msg').textContent = 'Game over! ❌';
-            document.querySelector('.ulc-player-score').textContent = playerScore;
-        }
+      wrongSound.play();
+      emptyNumMsg();
+      printScore();
+    } else if (playerScore === 0 && guessNum !== cpuNum) {
+      wrongSound.play();
+      gameOverMsg();
+      disableCheckBtn();
     } else if (guessNum < cpuNum) {
-        // Run only if playerScore is greater than zero
-        if (playerScore > 1) {
-            document.querySelector('#ulc-play-msg').textContent = 'Yikes!!! Too Low 🥴';
-            // Decrease score if guess is wrong
-            playerScore--
-            document.querySelector('.ulc-player-score').textContent = playerScore;
-        } else {
-            playerScore--
-            document.querySelector('#ulc-play-msg').textContent = 'Game over! ❌';
-            document.querySelector('.ulc-player-score').textContent = playerScore;
-        }
+      wrongSound.play();
+      subScore();
+      printScore();
+      lowNumMsg();
+      guessNum = Number(document.getElementById("ulc-num-input").value);
+    } else if (guessNum > cpuNum) {
+      wrongSound.play();
+      subScore();
+      printScore();
+      highNumMsg();
+      guessNum = Number(document.getElementById("ulc-num-input").value);
+    } else if (guessNum === cpuNum) {
+      correctSound.play();
+      disableCheckBtn();
+      guessPlaceholder = document.getElementsByClassName(
+        "ulc-number"
+      )[0].textContent = guessNum;
+      addScore();
+      printScore();
+      correctGuessMsg();
+      correctGuessBg();
+      setTimeout(() => {
+        normalBgColor();
+        enableCheckBtn();
+      }, 3500);
+      if (playerScore > highScore) {
+        highScore = playerScore;
+        document.getElementById("ulc-player-highscore").textContent = highScore;
+      }
+      cpuNum = Math.trunc(Math.random() * 20) + 1;
+      // guessNum = Number(document.getElementById("ulc-num-input").value);
+      document.getElementById("ulc-num-input").value = "";
     }
-});
-
+  });
 
 // Reset game function
-document.querySelector('.reset').addEventListener('click', function () {
-    // Reset player score
-    playerScore = 20;
-    document.querySelector('.ulc-player-score').textContent = playerScore;
-    // Set guessed number placeholder
-    document.querySelector('.ulc-number').textContent = '?';
-    // Reset game message
-    document.querySelector('#ulc-play-msg').textContent = 'Start Guessing...';
-    // Reset player input field
-    document.querySelector('#ulc-num-input').textContent = '';
-})
+document.querySelector(".reset").addEventListener("click", function () {
+  // Reset player score
+  playerScore = 100;
+  highScore = 0;
+  document.getElementsByClassName("ulc-player-score")[0].textContent =
+    playerScore;
+  // Set guessed number placeholder
+  document.getElementsByClassName("ulc-number")[0].textContent = "?";
+  // Reset game message
+  document.getElementById("ulc-play-msg").textContent = "Start guessing...";
+  // Reset player input field
+  document.getElementById("ulc-num-input").value = "";
+});
