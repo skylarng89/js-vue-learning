64700453683a45ba180a5f90ba34dd78c782fbb1
Patrick Aziken <onaziken@gmail.com>
2023-01-11 01:04:39 +0100

    Attribute binding

---
+++ 07_vue_mastery/0002_attribute_binding/assets/app.js
@@ -0,0 +1,10 @@
+const vueApp = Vue.createApp({
+    data() {
+        return {
+            prodName: 'Nike Jordan Max Aura 4',
+            prodShortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et egestas lectus, quis porttitor sapien. Nullam vehicula vehicula orci, sed ornare nisl iaculis nec. Nam vel ornare augue.',
+            prodMainImg: './assets/images/nike_jordan_max_aura_4_black_red_strip.webp',
+            prodSourceUrl: 'https://www.nike.com/t/jordan-max-aura-4-mens-shoes-sDVD5R/DN3687-006'
+        }
+    }
+})
